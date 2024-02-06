a19f2547a79c3d7dec8fc88812adb9d784c52e78
caniceiro <caniceiro@dei.uc.pt>
2024-02-06 14:20:22 +0000

    Adição backend

--- Project3-Backend/src/main/java/aor/paj/project3backend/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package aor.paj.project3backend;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
