5dedec837857652104349245b09bf66e913a90bb
canicoman <74936741+canicoman@users.noreply.github.com>
2024-02-06 13:53:10 +0000

    Adição Backend

---
+++ Project3-Backend/src/main/java/aor/paj/project3backend/HelloApplication.java
@@ -0,0 +1,9 @@
+package aor.paj.project3backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
