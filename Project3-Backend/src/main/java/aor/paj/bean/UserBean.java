91db16dc8f696e96537562599e3c8250cd77b83e
caniceiro <caniceiro@dei.uc.pt>
2024-02-08 10:34:39 +0000

    Começaremos a adicionar comentários melhores assim que a aplicação estiver um pouco mais funcionar e orientada Alteração nome Projeto no POM
    @ApplicationScoped tem de estar nna appDataManager.. acabo por ser um bean=!?!
    
    A partida o user está funcional

--- Project3-Backend/src/main/java/aor/paj/bean/UserBean.java
+++ Project3-Backend/src/main/java/aor/paj/bean/UserBean.java
@@ -1,25 +1,57 @@
 package aor.paj.bean;
 
-import aor.paj.AppDataManager;
 import aor.paj.dto.User;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbConfig;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 
 @ApplicationScoped
 public class UserBean {
-    @Inject
-    AppDataManager appDataManager;
+    final String filename = "allUser.json";
 
+    private ArrayList<User> users;
     public UserBean() {
-
+        File f = new File(filename);
+        if(f.exists()){
+            try {
+                FileReader filereader = new FileReader(f);
+                users = JsonbBuilder.create().fromJson(filereader, new
+                        ArrayList<User>() {}.getClass().getGenericSuperclass());
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }else{
+            users = new ArrayList<User>();
+        }
     }
     public boolean userExists(String username, String email){
-        return appDataManager.confirmUserExists(username,email);
+        for(User us : users){
+            if(us.getEmail().equals(email) || us.getUsername().equals(username)){
+                return true;
+            }
+        }
+        return false;
     }
-    public void addUser(User user) {
 
-        appDataManager.addUser(user);
+    public void addUser(User a) {
+        users.add(a);
+        writeIntoJsonFile();
     }
-
+    private void writeIntoJsonFile(){
+        Jsonb jsonb = JsonbBuilder.create(new
+                JsonbConfig().withFormatting(true));
+        try {
+            jsonb.toJson(users, new FileOutputStream(filename));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
