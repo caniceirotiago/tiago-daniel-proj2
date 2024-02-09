3eeba8daa435015a546c851ed6aecb452727882e
Tiago <caniceirotiago@gmail.com>
2024-02-09 00:44:16 +0000

    sd

---
+++ Project3-Frontend/js/UserPhoto.js
@@ -0,0 +1,7 @@
+export function loadPhoto(){
+    const photoUrl = localStorage.getItem("photoUrl");
+    if(photoUrl !== null){
+        const userPhoto = document.getElementById("userPhoto");
+        userPhoto.setAttribute("src", photoUrl);
+    }
+}
