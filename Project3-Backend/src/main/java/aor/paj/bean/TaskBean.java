d0ea7234722d64ffd3e25c7c4df0f9e9c72485b4
Daniel Alves da Silva <danielfilipsilva@gmail.com>
2024-02-10 01:16:19 +0000

    routes da api para task. edição inacabada, e rever o que envolve hashmaps.

--- Project3-Backend/src/main/java/aor/paj/bean/TaskBean.java
+++ Project3-Backend/src/main/java/aor/paj/bean/TaskBean.java
@@ -1,67 +1,128 @@
 package aor.paj.bean;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.*;
 
 import aor.paj.dto.Task;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.bind.JsonbConfig;
+
 @ApplicationScoped
 public class TaskBean {
-    final String filename = "activities.json";
+    final String filename = "allTasks.json";
+
+    // guarda todas as tasks
     private ArrayList<Task> tasks;
+    // estabelece relação entre utilizador e quais as suas tasks
+    // chave: username, valor: lista de tasks
+    // implementado como um HashMap para possibilitar no futuro ter mais do que uma tasks por utilizador
+    // o hashmap não precisa de ser guardado em ficheiro, pq é reconstruido a partir da lista de tasks
+
+    private Map<String, ArrayList<Task>> userTasksMap;
+
+
     public TaskBean() {
         File f = new File(filename);
-        if(f.exists()){
+        if (f.exists()) {
             try {
                 FileReader filereader = new FileReader(f);
+                // reconstruir a lista de tasks a partir do ficheiro
                 tasks = JsonbBuilder.create().fromJson(filereader, new
-                        ArrayList<Task>() {}.getClass().getGenericSuperclass());
+                        ArrayList<Task>() {
+                        }.getClass().getGenericSuperclass());
+
+                // reconstruir o hashmap a partir da lista de atividades
+                userTasksMap = new HashMap<>();
+                for (Task a : tasks) {
+                    if (userTasksMap.containsKey(a.getUsername())) {
+                        userTasksMap.get(a.getUsername()).add(a);
+                    } else {
+                        ArrayList<Task> userTasks = new ArrayList<>();
+                        userTasks.add(a);
+                        userTasksMap.put(a.getUsername(), userTasks);
+                    }
+
+                }
+
             } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
             }
-        }else
+        } else
             tasks = new ArrayList<Task>();
+        userTasksMap = new HashMap<>();
+
     }
-    public void addActivity(Task a) {
+
+    public void addTask(Task a) {
+        // adiciona a task à lista de tasks
         tasks.add(a);
+        // adiciona a task à lista de tasks do utilizador correspondente no hashmap ou cria uma nova lista
+        if (userTasksMap.containsKey(a.getUsername())) {
+            userTasksMap.get(a.getUsername()).add(a);
+        } else {
+            ArrayList<Task> userTasks = new ArrayList<>();
+            userTasks.add(a);
+            userTasksMap.put(a.getUsername(), userTasks);
+        }
+        // atualiza o ficheiro de tasks
         writeIntoJsonFile();
+
+
     }
-    public Task getActivity(int i) {
+
+    public Task getTask(String id) {
         for (Task a : tasks) {
-            if (a.getId() == i)
+            if (a.getId().equals(id))
                 return a;
         }
         return null;
     }
-    public ArrayList<Task> getActivities() {
+
+    public ArrayList<Task> getTasks() {
         return tasks;
     }
-    public boolean remoreActivity(int id) {
-        for (Task a : tasks) {
-            if (a.getId() == id) {
-                tasks.remove(a);
-                return true;
-            }
-        }
-        return false;
+
+    // getter do hashmap
+    public Map<String, ArrayList<Task>> getUserTasksMap() {
+        return userTasksMap;
     }
-    public boolean updateActivity(int id, Task activity) {
+
+    // obter lista de tarefas por user
+    public ArrayList<Task> getAllTasksByUser(String user) {
+        return userTasksMap.get(user);
+    }
+
+    public boolean removeTask(String id) {
         for (Task a : tasks) {
-            if (a.getId() == id) {
-                a.setTitle(activity.getTitle());
-                a.setDescription(activity.getDescription());
+            if (a.getId().equals(id)) {
+                tasks.remove(a);
+                // Atualiza o HashMap
+                getUserTasksMap().get(a.getUsername()).remove(id);
+                if (getUserTasksMap() != null) {
+                    getUserTasksMap().remove(a);
+                    if (getUserTasksMap().isEmpty()) {
+                        userTasksMap.remove(a.getUsername());
+                    }
+                }
+                // atualiza o ficheiro de tasks
                 writeIntoJsonFile();
                 return true;
             }
         }
+        // não encontrou a task
         return false;
     }
-    private void writeIntoJsonFile(){
+
+    ////// FALTA O MÉTODO PARA O UPDATE DA TASK
+
+
+
+    private void writeIntoJsonFile() {
         Jsonb jsonb = JsonbBuilder.create(new
                 JsonbConfig().withFormatting(true));
         try {
--- Project3-Backend/src/main/java/aor/paj/service/TaskService.java
+++ Project3-Backend/src/main/java/aor/paj/service/TaskService.java
@@ -1,6 +1,7 @@
 package aor.paj.service;
 import java.util.List;
 import aor.paj.bean.TaskBean;
+import aor.paj.bean.UserBean;
 import aor.paj.dto.Task;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
@@ -19,39 +20,90 @@ import jakarta.ws.rs.Path;
 public class TaskService {
     @Inject
     TaskBean taskBean;
+    @Inject
+    UserBean userBean;
 
-    @POST
-    @Path("/add")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response addActivity(Task a) {
-        taskBean.addActivity(a);
-        return Response.status(200).entity("A new activity is created").build();
-    }
+    // NOTA :  a classe UserSession não está a ser usada, pq não é necessário
+    // pq o user e pass está a ser passado no header através do sessionstorage,
+    // vamos apenas comparar se o user que está a tentar fazer a ação é o mesmo que está logado
+    // se não for, não deixa fazer a ação, sem alterar classes de estado de sessão em back end
+    // porque isto é uma API RESTFUL, logo stateless, não pode ter estados
+
+
+    // tudo isto é em relação a cada USER
+    // OS ENDPOINTS são apenas para o user que está logado
+    // para serem mostradas todas as tarefas , cada user teria acesso a tarefas
+    // de outros users, o que não é suposto. quando existir user admin, cria-se esse endpoint especifico
+
+
+    //R6 - List tasks of a user
+
+    // fazer um get de todas as tarefas de um user, com um filtro por user
+    // o path será /rest/task/all/query?user={username}
+    // o método será GET
+    // o output será um JSON com todas as tarefas desse user, validado
+    // o status code será 200 (no ok), 401 (nao autenticado) e 403 (não autorizado)
+
     @GET
-    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getActivity(@PathParam("id")int id) {
-        Task activity = taskBean.getActivity(id);
-        if (activity==null)
-            return Response.status(200).entity("Activity with this idea is not found").build();
-        return Response.status(200).entity(activity).build();
+    @Path("/all")
+    public Response getAllTasksByUser(@HeaderParam("username") String username, @HeaderParam("password") String password, String user) {
+        if (username == null || password == null)
+            return Response.status(401).entity("{\"Error\":\"User not logged in\"}").build();
+        else if (userBean.loginConfirmation(username, password)) {
+            return Response.ok(taskBean.getAllTasksByUser(user)).build();
+        } else
+            return Response.status(403).entity("{\"Error\":\"User permissions violated. Can't see tasks of other users\"}").build();
+
     }
+
+    // editar task - Change task of user tasks
+    @PUT
+    @Path("/edit")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response editTask(@HeaderParam("username") String username, @HeaderParam("password") String password, Task a) {
+        if (username == null || password == null)
+            return Response.status(401).entity("{\"Error\":\"User not logged in\"}").build();
+        else if (userBean.loginConfirmation(username, password) && username.equals(a.getUsername())) {
+            // taskBean.editTask(a);
+            // chamar get task atualizada
+            return Response.status(200).entity("Task edited").build();
+        } else
+            return Response.status(403).entity("{\"Error\":\"User permissions violated. Can't edit tasks of other users\"}").build();
+    }
+
+
+    // adicionar task
+    //R9 - Add task to user tasks
+    // retornar o 200, o não autenticado (401) e o nao autorizado (403) , qd tenta outro user
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createTask(@HeaderParam("username") String username, @HeaderParam("password") String password, Task a) {
+        if (username == null || password == null)
+            return Response.status(401).entity("{\"Error\":\"User not logged in\"}").build();
+        else if (userBean.loginConfirmation(username, password) && username.equals(a.getUsername())) {
+            taskBean.addTask(a);
+            return Response.status(201).entity("A new task has been created").build();
+        } else
+            return Response.status(403).entity("{\"Error\":\"User permissions violated. Can't add tasks to other users\"}").build();
+    }
+
+
+    // R10 - Delete task of user tasks
+    //o status code será 200 (no ok), 401 (nao autenticado) e 403 (não autorizado)
     @DELETE
     @Path("/delete")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response removeActivity(@QueryParam("id")int id) {
-        boolean deleted = taskBean.remoreActivity(id);
-        if (!deleted)
-            return Response.status(200).entity("Activity with this idea is not found").build();
-        return Response.status(200).entity("deleted").build();
-    }
-    @PUT
-    @Path("/update")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response updateActivity(Task a, @HeaderParam("id") int id) {
-        boolean updated = taskBean.updateActivity(id, a);
-        if (!updated)
-            return Response.status(200).entity("Activity with this idea is not found").build();
-        return Response.status(200).entity("updated").build();
+    public Response removeTask(@HeaderParam("username") String username, @HeaderParam("password") String password, @QueryParam("id") String id) {
+        if (username == null || password == null)
+            return Response.status(401).entity("{\"Error\":\"User not logged in\"}").build();
+        else if (userBean.loginConfirmation(username, password) && username.equals(taskBean.getTask(id).getUsername())) {
+            boolean deleted = taskBean.removeTask(id);
+            if (!deleted)
+                return Response.status(404).entity("{\"Error\":\"Task not found\"}").build();
+            return Response.status(200).entity("Task deleted").build();
+        } else
+            return Response.status(403).entity("{\"Error\":\"User permissions violated. Can't delete tasks of other users\"}").build();
     }
 }
