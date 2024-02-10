a4ee6a94d6cbcbd981b15177026c520b4b35ee0f
Tiago <caniceirotiago@gmail.com>
2024-02-10 13:18:05 +0000

    Neste momento está a criar ficheiro Json com parametros do user do frontend para o backend Não deixar criar dois utilizadores iguais
    mudei o nome da Appmanager e mudei para outro sitioplementação de logout
    
    Get de foto já está feito no backend falta no frontend
    Nesto momento a foto está a funcionar notar que é necessário espandir o campo de registo para aceitar mais caracteres
    Quase tudo feito na parte do utilizador.. apenas falta fazer a funcionalidade de alterar a password

--- Project3-Backend/src/main/java/aor/paj/dto/Task.java
+++ Project3-Backend/src/main/java/aor/paj/dto/Task.java
@@ -1,25 +1,52 @@
+/**Código Daniel*/
+
 package aor.paj.dto;
+import jakarta.validation.constraints.NotNull;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 @XmlRootElement
 public class Task {
-    int id;
-    String title;
-    String description;
+
+    // todos os atributos têm de ser válidos
+    // o id é gerado automaticamente e é único
+    // o username associado e o ID não podem ser alterados, logo não têm setters
+
+    @NotNull
+    private String id;
+    @NotNull
+    private String title;
+    @NotNull
+
+    private String description;
+    @NotNull
+    private int priority;
+    @NotNull
+    private String status;
+
+    @NotNull
+    private String username;
+
+
     public Task() {
     }
-    public Task(int i, String t, String d) {
-        this.id=i;
+    public Task(String t, String d, int p, String s, String u) {
+        this.id= UUID.randomUUID().toString();
         this.title = t;
         this.description= d;
+        this.priority = p;
+        this.status = s;
+        this.username = u;
+
     }
     @XmlElement
-    public int getId() {
+    public String getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
-    }
+
     @XmlElement
     public String getTitle() {
         return title;
@@ -33,5 +60,39 @@ public class Task {
     }
     public void setDescription(String description) {
         this.description = description;
+    }
+    @XmlElement
+    public int getPriority() {
+        return priority;
+    }
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+    @XmlElement
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    @XmlElement
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", priority=" + priority +
+                ", status='" + status + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+
     }
 }
