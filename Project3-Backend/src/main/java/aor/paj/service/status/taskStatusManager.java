c3b521610e2b4b6e63af969003cd7a74b648abab
Tiago <caniceirotiago@gmail.com>
2024-02-17 17:12:48 +0000

    alt

---
+++ Project3-Backend/src/main/java/aor/paj/service/status/taskStatusManager.java
@@ -0,0 +1,14 @@
+package aor.paj.service.status;
+
+import java.util.ArrayList;
+
+public class taskStatus {
+    public static final int TODO = 100;
+    public static final int DOING = 200;
+    public static final int DONE = 300;
+
+    public taskStatus() {
+    }
+
+
+}
